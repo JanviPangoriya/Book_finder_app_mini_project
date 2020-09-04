e9060d5c53369fb0bfa8523cfd27429cb394b905
JanviPangoriya <janvi.pangoriya_cs18@gla.ac.in>
2020-09-04 23:03:39 +0530

    Forget €password Activity created

---
+++ app/src/main/java/com/miniproject/bookapp/ForgetPassword.kt
@@ -0,0 +1,11 @@
+package com.miniproject.bookapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ForgetPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}

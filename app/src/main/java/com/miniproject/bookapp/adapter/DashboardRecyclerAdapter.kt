07a07eeca3512648197c27a1dbf3b6db704720c8
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-11-06 22:11:06 +0530

    Dashboard Recycler View Updated

--- app/src/main/java/com/miniproject/bookapp/adapter/DashboardRecyclerAdapter.kt
+++ app/src/main/java/com/miniproject/bookapp/adapter/DashboardRecyclerAdapter.kt
@@ -5,28 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.miniproject.bookapp.R
-import kotlinx.android.synthetic.main.recycler_dashboard_single_row.view.*
+import com.miniproject.bookapp.model.DashboardParentItem
 
-class DashboardRecyclerAdapter(val context:Context,val itemList:ArrayList<String>):
+class DashboardRecyclerAdapter(val context: Context, val itemItem: ArrayList<DashboardParentItem>) :
     RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>() {
-    class DashboardViewHolder(view: View):RecyclerView.ViewHolder(view){
-        val textView:TextView=view.findViewById(R.id.txtBookName)
+
+    private var viewPool = RecyclerView.RecycledViewPool()
+
+    class DashboardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var categoryName: TextView = view.findViewById(R.id.categoryName)
+        var childRecycler: RecyclerView = view.findViewById(R.id.childRecycler)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.recycler_dashboard_single_row,parent,false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_dashboard_parent, parent, false)
         return DashboardViewHolder(view)
     }
 
-    override fun getItemCount(): Int{
-        return itemList.size
+    override fun getItemCount(): Int {
+        return itemItem.size
     }
 
     override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
-        val text=itemList[position]
-        holder.textView.text=text
+        val parentItem = itemItem[position]
+        holder.categoryName.text = parentItem.categoryName
+        val layoutManager =
+            LinearLayoutManager(holder.childRecycler.context, LinearLayoutManager.HORIZONTAL, false)
+        layoutManager.initialPrefetchItemCount = parentItem.childItemItem.size
+        val childItemAdapter = DashboardChildItemRecyclerAdapter(context, parentItem.childItemItem)
+        holder.childRecycler.layoutManager = layoutManager
+        holder.childRecycler.adapter = childItemAdapter
+        holder.childRecycler.setRecycledViewPool(viewPool)
     }
 
 }

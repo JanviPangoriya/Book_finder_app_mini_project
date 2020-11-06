b74298a152879501a84a1f52db36b219157f669f
bgautam9045 <bgautam9045@gmail.com>
2020-11-06 18:35:58 +0530

    dashboardRecyclerView_Layout

--- app/src/main/java/com/miniproject/bookapp/fragment/DashboardFragment.kt
+++ app/src/main/java/com/miniproject/bookapp/fragment/DashboardFragment.kt
@@ -1,20 +1,36 @@
 package com.miniproject.bookapp.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.miniproject.bookapp.R
+import com.miniproject.bookapp.adapter.DashboardRecyclerAdapter
 
 class DashboardFragment : Fragment() {
+
+    lateinit var recyclerDashboard:RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var recyclerAdapter: DashboardRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        layoutManager=LinearLayoutManager(activity)
+
+
+       recyclerDashboard.addItemDecoration(
+           DividerItemDecoration(recyclerDashboard.context,(layoutManager as LinearLayoutManager).orientation)
+       )
         return view
     }
 
